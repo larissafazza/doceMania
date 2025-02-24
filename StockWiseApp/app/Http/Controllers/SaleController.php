4fee8736e4ab64f4615b1ec384d4bbb90011e9f1
Caio Vincenzo <reisdima@hotmail.com>
2025-02-24 10:23:43 -0300

    structure requests and redirects to sales views

--- StockWiseApp/app/Http/Controllers/SaleController.php
+++ StockWiseApp/app/Http/Controllers/SaleController.php
@@ -7,59 +7,74 @@ use Illuminate\Http\Request;
 
 class SaleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        //NOTE: criar filtros por dia, metodo de pagamento, horários e valor
+        $sales = Sales::all();
+        return view('sales.index', compact('sales'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+        //NOTE: verificar se o produto está disponível e se o estoque é suficiente, torná-lo disabled se não estiver
+        //fazer modal? ou colocar na própria index?
+        $products = Product::all();
+        return view('sales.create', compact('products'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $user_id = Auth::id();
+
+        $report = Report::whereDate('generated_at', Carbon::now()->toDateString())->first();
+        $report_id = $report ? $report->id : null;
+
+        $request->validate([
+            'client_name' => 'string',
+            'total_cost' => 'required|numeric',
+            'payment_method' => 'required|string',
+        ]);
+
+        $data = [
+            'client_name' => $request->client_name,
+            'total_cost' => $request->total_cost,
+            'payment_method' => $request->payment_method,
+            'user_id' => $user_id,
+            'report_id' => $report_id
+        ];
+
+        $product->quantity -= $request->quantity;
+        $product->save();
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Sale $sale)
     {
-        //
+        //NOTE: FAZER UMA MODAL APENAS
+        return view('sales.show', compact('sale'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Sale $sale)
     {
-        //
+        //NOTE: FAZER UMA MODAL APENAS ?
+        return view('sales.edit', compact('sale'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Sale $sale)
     {
-        //
+        $request->validate([
+            'client_name' => 'string',
+            'total_cost' => 'required|numeric',
+            'payment_method' => 'required|string',
+        ]);
+
+        $$sale->update($request->all());
+
+        return redirect()->route('sales.index')->with('success', 'Product updated successfully');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Sale $sale)
     {
-        //
+        $sale->delete();
+        return redirect()->route('sales.index')->with('success', 'Product deleted successfully');
     }
 }
