e3d475ed5afa17c8f943904523dec977ab2568d8
Caio Vincenzo <reisdima@hotmail.com>
2025-02-23 21:07:17 -0300

    Create initial structure based on classes diagram

---
+++ StockWiseApp/database/migrations/2025_02_23_193353_create_reports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->dateTime('generated_at');
+            $table->float('total_credit', 4, 2);
+            $table->float('total_debit', 4, 2);
+            $table->float('total_card', 4, 2);
+            $table->float('total money', 4, 2);
+            $table->float('total_pix', 4, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reports');
+    }
+};
---
+++ StockWiseApp/database/migrations/2025_02_23_193448_create_sales_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->string('client_name');
+            $table->dateTime('date_time');
+            $table->float('total_cost', 4, 2);
+            $table->string('payment_method');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('report_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
