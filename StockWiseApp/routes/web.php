c4ea6867487821ee16afed228dd0174cc08821da
Caio Vincenzo <reisdima@hotmail.com>
2025-02-25 14:40:13 -0300

    add initial routes

--- StockWiseApp/routes/web.php
+++ StockWiseApp/routes/web.php
@@ -16,3 +16,30 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+// Route::get('/', function () {
+//     return view('auth.login');
+// });
+
+Route::resource('products', ProductController::class)
+    ->middleware('auth');
+    // ->except(['edit', 'update', 'destroy']);
+
+Route::resource('reports', ReportController::class)
+    ->middleware('auth');
+
+Route::resource('sales', SaleController::class)
+    ->middleware('auth');
+
+Route::resource('suppliers', SupplierController::class)
+    ->middleware('auth');
+
+// Route::put('todos/{id}/mark-as-done', [TodoController::class, 'markAsDone'])
+//     ->name('todos.markAsDone')
+//     ->middleware('auth');
+
+
+// Route::get('/home', [HomeController::class, 'index'])
+//     ->name('home')
+//     ->middleware('auth');
