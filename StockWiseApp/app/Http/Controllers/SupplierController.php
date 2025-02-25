f1192ba724b375684e6fcf5c52f32f648ef5501d
Caio Vincenzo <reisdima@hotmail.com>
2025-02-25 14:31:16 -0300

    structure requests and redirects to suppliers views

--- StockWiseApp/app/Http/Controllers/SupplierController.php
+++ StockWiseApp/app/Http/Controllers/SupplierController.php
@@ -7,59 +7,56 @@ use Illuminate\Http\Request;
 
 class SupplierController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $suppliers = Supplier::all();
+        return view('suppliers.index', compact('suppliers'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+        $products = Product::all();
+        return view('products.edit', compact('products'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => ['required','string','max:255'],
+            'seller' => ['required','string','max:255'],
+            'address' => ['required','string','max:255'],
+            'cellphone' => ['required','string','max:255']
+        ]);
+
+        Supplier::create($request->all());
+
+        return redirect()->route('suppliers.index')->with('success', 'Fornecedor criado com sucesso!');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Supplier $supplier)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Supplier $supplier)
     {
-        //
+        $products = Product::all();
+        return view('products.edit', compact('products', 'suppliers'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Supplier $supplier)
     {
-        //
+        $request->validate([
+            'name' => ['required','string','max:255'],
+            'seller' => ['required','string','max:255'],
+            'address' => ['required','string','max:255'],
+            'cellphone' => ['required','string','max:255']
+        ]);
+
+        $supplier->update($request->all());
+
+        return redirect()->route('suppliers.index')->with('success', 'Fornecedor atualizado com sucesso!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Supplier $supplier)
     {
-        //
+        $supplier->delete();
+
+        return redirect()->route('suppliers.index')->with('success', 'Fornecedor excluído com sucesso!');
     }
 }
