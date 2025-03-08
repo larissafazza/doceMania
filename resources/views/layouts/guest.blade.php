849735bc501643c15f029b6e8238d6bb947d9a9c
Caio Vincenzo <reisdima@hotmail.com>
2025-03-08 19:57:40 -0300

    create intial login/register/profile feature

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Doce Mania') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -14,23 +14,16 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
+    <body class="font-sans text-gray-900 antialiased">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0" style="background-color:#14bfd3">
+            <div>
+                <a href="/">
+                    <img src="{{ asset('img/logo-nav.png') }}" alt="Logo" class="h-60">
+                </a>
+            </div>
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
-            </main>
+            </div>
         </div>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\SaleController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,29 +14,18 @@ use App\Http\Controllers\SaleController;
 |
 */
 
-// Auth::routes();
-// Route::get('/', function () {
-//     return view('products.index');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::resource('products', ProductController::class);
-    // ->middleware('auth');
-    // ->except(['edit', 'update', 'destroy']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('reports', ReportController::class);
-    // ->middleware('auth');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::resource('sales', SaleController::class);
-    // ->middleware('auth');
-
-Route::resource('suppliers', SupplierController::class);
-    // ->middleware('auth');
-
-// Route::put('todos/{id}/mark-as-done', [TodoController::class, 'markAsDone'])
-//     ->name('todos.markAsDone')
-//     ->middleware('auth');
-
-
-// Route::get('/home', [HomeController::class, 'index'])
-//     ->name('home')
-//     ->middleware('auth');
+require __DIR__.'/auth.php';
