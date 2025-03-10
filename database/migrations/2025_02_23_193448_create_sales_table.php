1f4bf567f1de4034f4be6a98be02efdb36468b08
Caio Vincenzo <reisdima@hotmail.com>
2025-03-09 21:21:53 -0300

    Remove report unnecessary feature

--- database/migrations/2025_02_23_193448_create_sales_table.php
+++ database/migrations/2025_02_23_193448_create_sales_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->float('total_cost', 6, 2);
             $table->string('payment_method');
             $table->foreignId('user_id')->constrained();
-            $table->foreignId('report_id')->constrained();
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,11 @@ use App\Http\Controllers\SupplierController;
 |
 */
 
-// Auth::routes();
-// Route::get('/', function () {
-//     return view('products.index');
-// });
-
-
 Route::resource('products', ProductController::class)
 ->middleware('auth');
 
-Route::resource('reports', ReportController::class)
-->middleware('auth');
+// Route::resource('reports', ReportController::class)
+// ->middleware('auth');
 Route::get('/report/missing', [ReportController::class, 'missing'])->name('reports.missing');
 Route::get('/report/missing/export-pdf', [ReportController::class, 'exportPdf'])->name('report.missing.export');
 Route::get('/report/invoicing', [ReportController::class, 'invoicing'])->name('reports.invoicing');
